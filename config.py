d2809980cee77f7c3840f13e6a3613d13964ba06
Cefer01234 <86205351+Cefer01234@users.noreply.github.com>
2021-09-01 23:14:38 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("2630368"))
+API_HASH = os.getenv("658ba9e18a4fec6bc76e2f87734d5514"))
+BOT_TOKEN = os.getenv("1970844353:AAEYu6nsDkeS6_slIHFu0w7VFCw8uHE8JLg"))
 DATABASE_URL = os.getenv("DATABASE_URL")
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
